953671235c280460dd935825cfb1bca00069ae99
Link4Electronics <link4electronics@gmail.com>
2026-07-01 08:52:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,35 +9,29 @@ echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     cmake          \
     fluidsynth     \
-    libdecor       \
     pipewire-audio \
     pipewire-jack  \
     sdl2_gfx       \
-    sdl2_image     \
     sdl2_mixer     \
     sdl2_ttf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini sdl2_image-mini
 
-# Comment this out if you need an AUR package
 make-aur-package sdl2 --noconfirm
 
-# If the application needs to be manually built that has to be done down here
 echo "Making nightly build of OpenClaw..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/pjasicek/OpenClaw"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-git clone "$REPO" ./OpenClaw
+git clone --depth 1 "$REPO" ./OpenClaw
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./OpenClaw
 mkdir -p build && cd build
-cmake .. \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 make -j$(nproc)
 cd ../Build_Release
 mv -v openclaw SAVES.XML clacon.ttf config.xml ASSETS.ZIP console02.tga ../../AppDir/bin
