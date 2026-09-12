d146937d3ccef0737abc5f8a46427efc9a933b47
Link4Electronics <link4electronics@gmail.com>
2026-09-12 01:56:26 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -31,9 +31,7 @@ echo "$VERSION" > ~/version
 mkdir -p ./AppDir/bin
 cd ./OpenClaw
 mkdir -p build && cd build
-cmake .. \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 make -j$(nproc)
 cd ../Build_Release
 mv -v openclaw SAVES.XML clacon.ttf config.xml ASSETS.ZIP console02.tga ../../AppDir/bin
